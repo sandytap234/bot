f1c009c85abc38ae6e05541722446636bd3080cf
sandytap234 <a6662287770@gmail.com>
2025-12-14 14:37:28 +0300

    new

---
+++ bot.py
@@ -0,0 +1,14 @@
+import os
+from aiogram import Bot, Dispatcher, executor, types
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=["start"])
+async def start(msg: types.Message):
+    await msg.answer("Бот работает на Render 🚀")
+
+if __name__ == "__main__":
+    executor.start_polling(dp)
